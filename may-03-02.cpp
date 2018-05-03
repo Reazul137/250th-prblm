7c8cd47391a5923bcedc72b42a974f7b538d8c70
Reazul Islam <reazulislam137@gmail.com>
2018-05-03 14:19:20 +0600

    first commit

---
+++ may-03-02.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+// This program shows how mixed expression work
+
+int main()
+{
+cout << 3 / 2 + 5.5 << endl;
+
+cout << 15.6 / 2.5 << endl;
+
+cout << 4 + 5 / 2.0 << endl;
+
+cout << 4 * 3 + 7 / 5 - 25.5 << endl;
+
+return 0;
+}
